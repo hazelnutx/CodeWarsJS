a8acff8c566a6ef1dc1a2dd57ac28d6c1205f80a
hazelnutx <raulrusu46@gmail.com>
2019-12-30 21:39:01 +0200

    finished the diff two array

--- FreeCodeCamp Intermediate Algo/Diff Two Arrays.js
+++ FreeCodeCamp Intermediate Algo/Diff Two Arrays.js
@@ -4,13 +4,11 @@
 // You can return the array with its elements in any order.
 
 function diffArray(arr1, arr2) {
-    let newArr = [];
-    arr1.filter(el => {
-        if (el !== arr2.indexOf(el)) {
-            newArr.push(arr2.indexOf(el));
-        }
-    });
-    return newArr;
+    return arr1
+        .concat(arr2)
+        .sort()
+        .filter(item => !arr1.includes(item) || !arr2.includes(item));
 }
 
 console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+console.log(diffArray([1, "calf", 3, "piglet"], [1, "calf", 3, 4]));
