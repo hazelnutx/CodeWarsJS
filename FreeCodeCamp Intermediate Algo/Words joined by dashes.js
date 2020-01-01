012980d5af25b049cae74d13d6efa7d87c9bd7b7
hazelnutx <raulrusu46@gmail.com>
2020-01-01 20:37:07 +0200

    how to split a sentece and check if there are whitespaces, or underscores and if there are words that have a capital letter space out between the lowercase letter and the capital letter and change the space to a dash -

---
+++ FreeCodeCamp Intermediate Algo/Words joined by dashes.js
@@ -0,0 +1,15 @@
+// Intermediate Algorithm Scripting: Spinal Tap Case
+
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+
+function spinalCase(str) {
+    return str
+        .split(/\s|_|(?=[A-Z])/)
+        .join("-")
+        .toLowerCase();
+}
+
+console.log(spinalCase("This Is Spinal Tap"));
+console.log(spinalCase("The_Andy_Griffith_Show"));
+console.log(spinalCase("Teletubbies say Eh-oh"));
+console.log(spinalCase("AllThe-small Things"));
